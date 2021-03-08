93e7f074f0b0ffe432917b0443f9a6babe4a03a4
92an <60664824+92an@users.noreply.github.com>
2021-03-08 18:54:23 +0100

    forms implementation learned

--- mysite/food/views.py
+++ mysite/food/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from .forms import ItemForm
+from django.shortcuts import redirect, render
 from django.http import HttpResponse
 from .models import Item
 from django.template import context, loader
@@ -23,4 +24,30 @@ def detail(request, item_id):
     }
     return render(request, 'food/detail.html', context)
 
+def create_item(request):
+    form = ItemForm(request.POST or None)
 
+    if form.is_valid():
+        form.save()
+        return redirect("food:index")
+
+    return render(request, "food/itemform.html", {"form":form})
+
+def edit_item(request, id):
+    item = Item.objects.get(id=id)
+    form = ItemForm(request.POST or None, instance=item)
+
+    if form.is_valid():
+        form.save()
+        return redirect("food:index")
+
+    return render(request, "food/itemform.html", {"form":form, "item":item})
+
+def delete_item(request, id):
+    item = Item.objects.get(id=id)
+
+    if request.method == "POST":
+        item.delete()
+        return redirect("food:index")
+
+    return render(request, "food/itemdelete.html", {"item":item} )
